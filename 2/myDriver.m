6d0e88ba9533c432f400c259bd8cfe8e2566c38b
riddhishb <riddhishb@gmail.com>
2015-02-07 00:37:02 +0530

    Question2 PartA Done, PartB half Done

--- 2/myDriver.m
+++ 2/myDriver.m
@@ -11,28 +11,26 @@ img = phantom(256);
 % The L parameter is tuned in the function PartA, which contains the
 % function myFilter12.
 
-figure;
-imagesc( out11 ); title('Manual filtering by Ram Lak, L=wmax')
+figure('Name','PartA: Ram Lak Filter');
+subplot(1,2,1);imagesc( out11 ); title('Manual filtering by Ram Lak, L=wmax')
 colormap(gray(256)); axis image; axis off
 
-figure;
-imagesc( out12 ); title('Manual filtering by Ram Lak, L=wmax/2')
+subplot(1,2,2);imagesc( out12 ); title('Manual filtering by Ram Lak, L=wmax/2')
 colormap(gray(256)); axis image; axis off
 
-figure;
-imagesc( out21 ); title('Manual filtering by Shepp-Logan, L=wmax')
+figure('Name','PartA: Shepp-Logan Filter');
+subplot(1,2,1);imagesc( out21 ); title('Manual filtering by Shepp-Logan, L=wmax')
 colormap(gray(256)); axis image; axis off
 
-figure;
-imagesc( out22 ); title('Manual filtering by Shepp-Logan, L=wmax/2')
+subplot(1,2,2);imagesc( out22 ); title('Manual filtering by Shepp-Logan, L=wmax/2')
 colormap(gray(256)); axis image; axis off
 
-figure;
-imagesc( out31 ); title('Manual filtering by Cosine Filter, L=wmax')
+figure('Name','PartA: Cosine Filter');
+subplot(1,2,1);imagesc( out31 ); title('Manual filtering by Cosine Filter, L=wmax')
 colormap(gray(256)); axis image; axis off
 
-figure;
-imagesc( out32 ); title('Manual filtering by Cosine Filter, L=wmax/2')
+
+subplot(1,2,2);imagesc( out32 ); title('Manual filtering by Cosine Filter, L=wmax/2')
 colormap(gray(256)); axis image; axis off
 
 % The diffrences in the images can be seen in the same manner and they are
@@ -44,3 +42,5 @@ colormap(gray(256)); axis image; axis off
 % colormap(gray(256)); axis image; axis off
 
 %% PartB: Backprojection of noisy image
+
+% [S0,S1,S5,R0,R1,R5,rrmse1,rrmse2,rrmse3] = partB(img)
