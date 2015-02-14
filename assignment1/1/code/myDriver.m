35f8c2009e025416c546449b7bd99b2e4af33dc8
Alankar Kotwal <alankarkotwal13@gmail.com>
2015-02-14 20:46:18 +0530

    q1 update

---
+++ assignment1/1/code/myDriver.m
@@ -0,0 +1,11 @@
+ % *******************************************************************************************
+% CS736 Assignment 1 Question 7
+% Alankar Kotwal <alankar.kotwal@iitb.ac.in>
+% Driver
+% Make sure the all fucntions are in the same directory as this file
+% Run the code after CDing to that directory.
+% *******************************************************************************************
+
+f = phantom(128);
+
+a = myIntegration(f, 0, 0, 1);
---
+++ assignment1/1/code/myRadonTrans.m
@@ -0,0 +1,10 @@
+% *******************************************************************************************
+% CS736 Assignment 1 Question 7
+% Alankar Kotwal <alankar.kotwal@iitb.ac.in>
+% myRadonTransform function
+% Make sure the driver is in the same directory as this file
+% Run the code after CDing to that directory.
+% *******************************************************************************************
+
+function myRadonTrans
+end
