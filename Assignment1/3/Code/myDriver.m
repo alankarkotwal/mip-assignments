3af945e5f09436bcb52be9a83f95124abcef3d09
riddhishb <riddhishb@gmail.com>
2015-02-15 13:18:43 +0530

    Question 3 Part A Done

---
+++ Assignment1/3/Code/myDriver.m
@@ -0,0 +1,12 @@
+
+tic;
+%% Riddhish Bhalodia and Alankar Kotwal
+
+% Part A function will give the output of D matrix estimate 
+% The input is the initial condition of L matrix
+% Here the inital condition is that L = [1,0;1,1] and D = LL'
+% Have applied Levenberg-marquadtt method for optimization
+
+D = partA(1,1,1);
+
+toc;
